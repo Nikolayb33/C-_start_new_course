0efcc574fed961862ec8743e3a10116b7d74deb2
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-14 09:38:09 +0300

    add task_1 in readme.md , Program.cs and modified .gitignore (add bin)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+int num = new Random().Next(10,100);
+
+Console.WriteLine(num);
+
+int num1= num/10;
+int num2= num %10;
+
+if(num1>num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
