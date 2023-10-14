a267983dbe8a22ab3cf64fea0005a0f79bda32ba
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-14 09:52:54 +0300

    add task_2 in Program.cs and README.md

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-int num = new Random().Next(10,100);
+// task 1
+// int num = new Random().Next(10,100);
+
+// Console.WriteLine(num);
+
+// int num1= num/10;
+// int num2= num %10;
+
+// if(num1>num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+
+//task_2
+// надо взять трехзначное чило и удалить вторую цифру
+int num = new Random().Next(100,1000);
 
 Console.WriteLine(num);
 
-int num1= num/10;
+int num1= num/100;
 int num2= num %10;
 
-if(num1>num2)
-{
-    Console.WriteLine(num1);
-}
-else
-{
-    Console.WriteLine(num2);
-}
+int result = num1*10+num2;
+
+Console.WriteLine(result);
